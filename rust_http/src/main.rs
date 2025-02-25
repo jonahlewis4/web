fd23c9e84817f4c3142bf33bdabd10d98515d8bf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-24 18:34:23 -0800

    add handler that creates a reader of the stream

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use std::{
+    io::BufReader,
+    net::{TcpListener, TcpStream}
+
+};
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
@@ -6,3 +10,10 @@ fn main() {
         println!("Connection incoming");
     }
 }
+
+fn handle_request(stream: TcpStream) {
+    let request = String::new();
+    let reader = BufReader::new(&stream);
+
+}
+
