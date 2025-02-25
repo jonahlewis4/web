f458507d5ae27153692c14b04378da8156f9a1bf
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-24 18:07:30 -0800

    borrowing

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -1,24 +1,12 @@
 fn main() {
-    let mut foo: i32 = 5;
-    let unsigned_foo: u32 = 10;
+    let s = String::from("Hello");
 
-    println!("Hello, world! foo: {} unsigned_foo: {}", foo, unsigned_foo);
+   foobar(&s);
 
-    foo = 7;
-    println!("Hello, world! foo: {} unsigned_foo: {}", foo, unsigned_foo);
-
-    foobar(5);
-
-
-    let x = 6;
-    match x {
-        1 => println!("one"),
-        2 | 3 | 4 | 5 => println!("2 | 3 | 4 | 5"),
-        _ => println!("Anything else"),
-    }
-
+    let s1 = s;
 }
 
-fn foobar(param : i8){
+
+fn foobar(param: &String){
     println!("{}", param);
 }
