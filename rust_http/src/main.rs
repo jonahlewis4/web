644f740a8a255f29b044f4e3d2930a511aaf1745
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-24 18:30:41 -0800

    listen to incoming requests

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -2,4 +2,7 @@ use std::net::TcpListener;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    for stream in listener.incoming() {
+        println!("Connection incoming");
+    }
 }
