74859cd98fd549e4501ec86ce8e6c7ba2b57d643
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-02-24 18:10:32 -0800

    create immutable refs

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -1,9 +1,12 @@
+//you can only use one mutable reference. You can have as many immutable references as you'd like
 fn main() {
     let s = String::from("Hello");
 
    foobar(&s);
 
-    let s1 = s;
+    let s1 = &s;
+    let s2 = &s;
+    let s3 = &s;
 }
 
 
