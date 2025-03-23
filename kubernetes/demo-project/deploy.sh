a1b69f9796ed9b978a0c279a50a2fd8841f709b6
Jonah Lewis <lewisj41@unlv.nevada.edu>
2025-03-22 22:17:23 -0700

    delete the kubernetes file

--- kubernetes/demo-project/deploy.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f mongo-config.yaml
-kubectl apply -f mongo-secret.yaml
-kubectl apply -f mongo.yaml
-kubectl apply -f webapp.yaml
--- kubernetes/demo-project/undeploy.sh
+++
@@ -1,4 +0,0 @@
-kubectl delete -f mongo-config.yaml
-kubectl delete -f mongo-secret.yaml
-kubectl delete -f mongo.yaml
-kubectl delete -f webapp.yaml
